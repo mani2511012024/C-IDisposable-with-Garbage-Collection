3d541f4218fce9a2052f336f106d3de88da7256b
Manikandan Krish <manikandan.krish@aspiresys.com>
2024-01-20 12:49:41 +0530

    application

---
+++ Dotnet Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class DisposableResources : IDisposable
+{
+    public DisposableResources()
+    {
+        Console.WriteLine("Resource acquired");    
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Resource Released");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        using (DisposableResources obj = new DisposableResources())
+        {
+            obj.Dispose();
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("End of the program");
+    }
+}
